9bc532acab88eda12d4d26c81ef33e0ea410c393
Walter Alexander Guerra Duque <3018766650101@ingenieria.usac.edu.gt>
2023-05-06 05:26:41 -0600

    Identificar server

--- ProyectoFase2/Frontend/cerberusPortal/src/config.js
+++ ProyectoFase2/Frontend/cerberusPortal/src/config.js
@@ -1,1 +1,2 @@
 export const ipBackend = "";
+export const PALABRA = "";
--- ProyectoFase2/Frontend/cerberusPortal/src/pages/Home.jsx
+++ ProyectoFase2/Frontend/cerberusPortal/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { PALABRA } from "../config.js"
 
 
 function Home() {
   return (
     <div className="main-content">
-      <h1>PROYECTO FINAL REDES</h1>
+      <h1>PROYECTO FINAL REDES </h1>
+      <h2>Server: {PALABRA}</h2>
       <img className="topo" src="https://res.cloudinary.com/alex4191/image/upload/v1664732563/practica%202/topologia_p2daff.png" alt="topologia"></img>
     </div>
   )
